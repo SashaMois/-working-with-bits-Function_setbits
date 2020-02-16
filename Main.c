699a8c406b8c54bf1ab4b045018fa1b30ba65e33
SashaMois <56739595+SashaMois@users.noreply.github.com>
2020-02-16 23:35:27 +0300

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,26 @@
+/* Function 'setbits' return argument 'x' that have 
+ * 'n' bits starting from 'p' pozition are equal 'n'
+ * extreme right bits of argument 'y' and the rest of
+ * the bits are untouched.
+ */
+
+#include <stdio.h>
+
+int setbits(x, p, n, y);
+
+int main()
+{
+    printf("%d\n", setbits(3, 3, 3, 11));
+
+    return 0;
+}
+
+int setbits(x, p, n, y)
+{
+    int a;
+    a = (y & ~(~0 << n)) << p; /* take extreme right bits of argument 'y' 
+                                      and move it on 'p' pozitions */
+    x = x | a;
+
+    return x;
+}
